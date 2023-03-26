78d2fca79a59c039b63e7b86121af5c02831c0cf
uttnaoki <uttnaoki@gmail.com>
2023-03-26 19:20:33 +0900

    データをDBから取得

--- src/server.js
+++ src/server.js
@@ -2,32 +2,26 @@ const { ApolloServer, gql } = require('apollo-server');
 const fs = require('fs');
 const path = require('path');
 
-// HackerNewsの１つ１つの投稿
-let links = [
-  {
-    id: 'link-0',
-    description: 'GraphQLチュートリアルをUdemyで学ぶ',
-    url: 'https://news.ycombinator.com/',
-  },
-];
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 // リゾルバ関数
 const resolvers = {
   Query: {
     info: () => 'HackerNewsクローン',
-    feed: () => links,
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany();
+    },
   },
   Mutation: {
-    post: (parent, args) => {
-      let idCount = links.length;
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      };
-
-      links.push(link);
-      return link;
+    post: (parent, args, context) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      });
+      return newLink;
     },
   },
 };
@@ -35,6 +29,9 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf-8'),
   resolvers,
+  context: {
+    prisma,
+  },
 });
 
 server
