d9de6dee99474601f0321ceeb9be080ee975036e
uttnaoki <uttnaoki@gmail.com>
2023-03-26 12:37:15 +0900

    add feed

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-## GraphQL の勉強
+# GraphQL の勉強
 
 - udemy
   - https://www.udemy.com/course/graphql-tutorial-with-newsapp-api/
+
+## メモ
+
+- ID は unique な String 型
--- src/server.js
+++ src/server.js
@@ -1,9 +1,25 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+// HackerNewsの１つ１つの投稿
+let links = [
+  {
+    id: 'link-0',
+    description: 'GraphQLチュートリアルをUdemyで学ぶ',
+    url: 'https://www.google.com/',
+  },
+];
+
 // GraphQLスキーマの定義
 const typeDefs = gql`
   type Query {
     info: String!
+    feed: [Link]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
@@ -11,6 +27,7 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     info: () => 'HackerNewsクローン',
+    feed: () => links,
   },
 };
 
